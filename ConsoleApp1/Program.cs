3ed0de50c759e03d5dc2a044421ab80263002056
Piotrek Dziubek <dziubekpiotrekofficial@gmail.com>
2020-06-10 19:02:48 +0200

    Add new Files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateRandom();
+        }
+        static void  GenerateRandom()
+        {
+            Console.WriteLine("Password Generator");
+            int min = 304;
+            int max = 1940;
+            Random _rdm = new Random();
+            int num = new Random().Next(1000, 9999);
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
